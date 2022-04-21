79541a388cf58c6ab9c756bccc1519b0496127a3
MS_PC <MS_PC@LAPTOP-3TJHGUDB>
2022-04-21 14:09:47 +0200

    api added

--- HttpClientDemo/Models/WeatherForecastModel.cs
+++ HttpClientDemo/Models/WeatherForecastModel.cs
@@ -5,19 +5,11 @@ namespace HttpClientDemo.Models
 
     public class WeatherForecastModel
     {
-        public DayForecastModel[] consolidated_weather { get; set; }
-        public DateTime sun_rise { get; set; }
-        public DateTime sun_set { get; set; }
-        public string title { get; set; }
-        public string timezone { get; set; }
-    }
-
-    public class DayForecastModel
-    {
-        public string weather_state_name { get; set; }
-        public string applicable_date { get; set; }
-        public float min_temp { get; set; }
-        public float max_temp { get; set; }
+        public DayForecastModel[] Consolidated_weather { get; set; }
+        public DateTime Sun_rise { get; set; }
+        public DateTime Sun_set { get; set; }
+        public string Title { get; set; }
+        public string Timezone { get; set; }
     }
 
 
